2cd3a05f4cd662ee78ace59bf6805ab324b4e7b9
Nighty <flander1059@gmail.com>
2025-09-01 16:36:33 +0300

    Add section 'Ready to use'

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,8 +1,14 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2,
-  spaceBetween: -200,
+  slidesPerView: 1,
+  spaceBetween: 20,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-custom-button-next",
+    prevEl: ".swiper-custom-button-prev",
   },
+  breakpoints: {
+    760: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+  }
 });
